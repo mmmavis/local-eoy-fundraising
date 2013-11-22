a016b2a11095dd44c980bde68f063bd25c705011
Mavis Ou <mmmavisss@gmail.com>
2013-11-21 16:28:18 -0800

    Shuffle layout a bit to solve the trapped gap problem

--- themes/OneMozilla-child/front-page.php
+++ themes/OneMozilla-child/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
     <div id="dashboard-main" role="main">
-      <div class="row">
+      <div class="row" id="first-row">
         <section class="white-bg two-columns">
         <h3>Total so far: <span id="period-graph-title"></span></h3>
         <div class="graph-box-container">
@@ -29,43 +29,45 @@
       </section>
       </div>
 
-      <div class="row">
-        <section class="white-bg two-columns">
-        <h3>View Source Fundraising Blog</h3>
-        <?php if ( dynamic_sidebar('view-blog-widget') ) : else : endif; ?>
-        <div id="latest-blog-summary">
-            <?php get_template_part( 'latest-post' ); ?>
+      <div class="row" id="second-row">
+        <section id="view-blog-section" class="white-bg">
+          <h3>View Source Fundraising Blog</h3>
+          <?php if ( dynamic_sidebar('view-blog-widget') ) : else : endif; ?>
+          <div id="latest-blog-summary">
+              <?php get_template_part( 'latest-post' ); ?>
+          </div>
+        </section>
+
+        <section id="poll-section" class="white-bg">
+          <h3>You Be the Fundraiser Poll</h3>
+          <?php if ( dynamic_sidebar('polls-widget') ) : else : endif; ?>
+        </section>
+
+        <section id="older-blog-post-section" class="white-bg">
+          <h3>Older Blog Posts</h3>
+          <?php if ( dynamic_sidebar('older-blog-post-widget') ) : else : endif; ?>
+        </section>
+
+        <div class="sub-row">
+          <section id="twitter-feeds" class="white-bg two-columns">
+            <h3>Twitter Feeds</h3>
+            <?php if ( dynamic_sidebar('twitter-feed-widget') ) : else : endif; ?>
+            <a class="twitter-timeline" href="https://twitter.com/search?q=%23lovetheweb+OR+%23protecttheweb" data-widget-id="401474842102202368" data-tweet-limit="1" data-chrome="noheader nofooter transparent noborders">Tweets about "#lovetheweb OR #protecttheweb"</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          </section>
+
+          <section id="links" class="white-bg two-columns">
+            <h3>Links</h3>
+            <a href="https://sendto.mozilla.org/" target="_blank">Donate Today</a><br/>
+            <!-- remember to pass the right page_id: (staging) 20; -->
+            <a href=<?php echo get_page_uri('20') ?> target="_blank">Campaign Overview</a><br/>
+            <a href="http://www.mozilla.org/en-US/about/manifesto/" target="_blank">Mozilla Manifesto</a><br/>
+            <a href="" target="">What is View-source Fundraising</a>
+          </section>
         </div>
-      </section>
-
-      <section class="white-bg two-columns">
-        <h3>You Be the Fundraiser Poll</h3>
-        <?php if ( dynamic_sidebar('polls-widget') ) : else : endif; ?>
-      </section>
       </div>
 
-      <div class="row">
-        <section id="older-blog-post" class="white-bg three-columns">
-        <h3>Older Blog Posts</h3>
-        <?php if ( dynamic_sidebar('older-blog-post-widget') ) : else : endif; ?>
-      </section>
 
-      <section id="twitter-feeds" class="white-bg three-columns">
-        <h3>Twitter Feeds</h3>
-        <?php if ( dynamic_sidebar('twitter-feed-widget') ) : else : endif; ?>
-        <a class="twitter-timeline" href="https://twitter.com/search?q=%23lovetheweb+OR+%23protecttheweb" data-widget-id="401474842102202368" data-tweet-limit="1" data-chrome="noheader nofooter transparent noborders">Tweets about "#lovetheweb OR #protecttheweb"</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-      </section>
-
-      <section id="links" class="white-bg three-columns">
-        <h3>Links</h3>
-        <a href="https://sendto.mozilla.org/" target="_blank">Donate Today</a><br/>
-        <!-- remember to pass the right page_id: (staging) 20; -->
-        <a href=<?php echo get_page_uri('20') ?> target="_blank">Campaign Overview</a><br/>
-        <a href="http://www.mozilla.org/en-US/about/manifesto/" target="_blank">Mozilla Manifesto</a><br/>
-        <a href="" target="">What is View-source Fundraising</a>
-      </section>
-      </div>
 
     </div><!-- #content-main -->
 <?php get_footer(); ?>
